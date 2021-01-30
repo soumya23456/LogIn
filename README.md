bfdb8ae20dcf1c91cd3b5f9e0cc7ece719085b78
soumya23456 <m.soumya234@gmail.com>
2021-01-30 19:40:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jcknak
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jcknak)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import LogIn from "./login";
+
+export default function App() {
+  return (
+    <div>
+      <LogIn />
+    </div>
+  );
+}
